016b11958bac8e08a9898b47b83dd0d01105cbe4
Iris Blanche <caprima.iris@gmail.com>
2016-09-08 22:54:06 +0200

    Code update

--- scripts/IB_EHT/main.lua
+++ scripts/IB_EHT/main.lua
@@ -51,10 +51,6 @@ function EHT.create()
 	data.hasStarterEffect = false
 	data.oldid = "NA"
 	
-	message.setHandler("getCallback", function(_,_,value)
-		data.callback = value
-	end)
-	
 	return data
 end
 
@@ -98,7 +94,6 @@ function EHT:IsInBiome(biome)
 	if b == nil then
 		-- property not set, get the mainbiome
 		local wt = "biome_" .. world.type()
-		sb.logInfo("biome_type: " .. wt)
 		if wt == biome then
 			world.setProperty("eht.biome", wt)
 			return true
