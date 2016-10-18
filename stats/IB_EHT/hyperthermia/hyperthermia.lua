d620a46f3783954be51a135e021c27bdaed7d51a
Iris Blanche <caprima.iris@gmail.com>
2016-10-18 16:35:22 +0200

    1.2.1 Adjustments
    - added RP codex recipe
    - added RP codex to shiplocker
    - added license info to hypo- and hyperthermia scripts
    - removed debug information from mainscript

--- stats/IB_EHT/hyperthermia/hyperthermia.lua
+++ stats/IB_EHT/hyperthermia/hyperthermia.lua
@@ -1,22 +1,48 @@
+--[[
+
+   Copyright 2016 Iris Blanche
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+
+]]
+
+-- Init
 function init()
-  self.energyCost = config.getParameter("energyCost", 1)
-  self.healthDamage = config.getParameter("healthDamage", 1)
-  script.setUpdateDelta(config.getParameter("tickRate", 1))
-  effect.addStatModifierGroup({{stat = "energyRegenPercentageRate", effectiveMultiplier = 0}})
+	self.energyCost = config.getParameter("energyCost", 1)
+	self.healthDamage = config.getParameter("healthDamage", 1)
+	script.setUpdateDelta(config.getParameter("tickRate", 1))
+	effect.addStatModifierGroup({
+		{
+			stat = "energyRegenPercentageRate",
+			effectiveMultiplier = 0
+		}
+	})
 end
 
+-- Update
 function update(dt)
-  mcontroller.controlModifiers(self.movementModifiers)
-  if (not status.overConsumeResource("energy", self.energyCost)) and self.healthDamage > 0 then
-    status.applySelfDamageRequest({
-        damageType = "IgnoresDef",
-        damage = self.healthDamage,
-        damageSourceKind = "fire",
-        sourceEntityId = entity.id()
-      })
-  end
+	mcontroller.controlModifiers(self.movementModifiers)
+	if (not status.overConsumeResource("energy", self.energyCost)) and self.healthDamage > 0 then
+		status.applySelfDamageRequest({
+			damageType = "IgnoresDef",
+			damage = self.healthDamage,
+			damageSourceKind = "fire",
+			sourceEntityId = entity.id()
+		})
+	end
 end
 
+-- Uninit
 function uninit()
 
 end
